cd46f1829c3628d66bd09c82e84d1e50569f6f7a
Lenovo <mahesh.smurthy@gmail.com>
2018-02-04 08:13:42 -0800

    Added gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,13 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
 
 var SOURCEPATH = {
     sassSource: 'src/scss/*.scss',
-    htmlSource: 'src/*.html'
+    htmlSource: 'src/*.html',
+    jsSource: 'src/js/*.js' // Having it as 'src/js/**' as path means everything in that folder 
 }
 
 var APPPATH = {
@@ -14,15 +17,31 @@ var APPPATH = {
     css: 'app/css',
     js: 'app/js'
 }
+
+gulp.task('clean-htmls', function(){
+    gulp.src(APPPATH.root+'/*.html',{read: false, force:true})
+        .pipe(clean());
+});
+
+gulp.task('clean-scripts', function(){
+    gulp.src(APPPATH.js+'/*.js', {read: false, forcr:true})
+        .pipe(clean());
+})
 // outputStyle Options - expanded, nested, compressed, compact
 gulp.task('sass', function () {
-    return gulp.src(SOURCEPATH.sassSource)
+    gulp.src(SOURCEPATH.sassSource)
         .pipe(autoprefixer())
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('copy', function() {
+gulp.task('copy-scripts',['clean-scripts'], function(){
+    gulp.src(SOURCEPATH.jsSource)
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest(APPPATH.js));
+});
+
+gulp.task('copy-htmls',['clean-htmls'], function() {
     gulp.src(SOURCEPATH.htmlSource)
         .pipe(gulp.dest(APPPATH.root));
 });
@@ -35,9 +54,10 @@ gulp.task('serve', ['sass'], function () {
     });
 });
 
-gulp.task('watch',['serve','copy'], function(){
+gulp.task('watch',['serve','copy-htmls', 'clean-htmls', 'copy-scripts', 'clean-scripts'], function(){
     gulp.watch([SOURCEPATH.sassSource],['sass']);
-    gulp.watch([SOURCEPATH.htmlSource],['copy']);
+    gulp.watch([SOURCEPATH.htmlSource],['copy-htmls']);
+    gulp.watch([SOURCEPATH.jsSource],['copy-scripts']);
 });
 
 gulp.task('default', ['watch']);
